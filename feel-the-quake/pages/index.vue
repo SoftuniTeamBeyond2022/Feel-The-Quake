a1432c3c97a79b84837e61ea65936e47b21ac0e8
Kardam Simeonov <masutayodagm@gmail.com>
2023-02-13 09:45:53 +0200

    initial commit

---
+++ feel-the-quake/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <img src="@/assets/images/redChina.webp" alt="redChina" />
+        <h1>Did you feel the quake?</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
