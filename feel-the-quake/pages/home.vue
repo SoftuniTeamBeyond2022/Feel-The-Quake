f7e226f32851cb0f8afafecd05de62531a9ebaec
Kardam Simeonov <masutayodagm@gmail.com>
2023-02-13 11:02:10 +0200

    home and index refinement

---
+++ feel-the-quake/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="h-screen bg-[#2CB13A] p-12">
+        <div class="mx-auto max-w-[17rem]">
+            <img src="@/assets/images/greenChina.jpg" alt="greenChina" />
+        </div>
+        <h1 class="text-white font-bold text-4xl text-center mt-12"></h1>
+        <button class="bg-green-300 p-2 w-full mt-14 text-2xl">   
+            What to do next
+        </button>
+        <button class="bg-yellow-300 p-2 w-full mt-4 text-2xl">   
+            Lorem Ipsum
+        </button>
+        <NuxtLink to="/">
+            <button class="bg-red-300 p-2 w-full mt-4 text-2xl">
+                Go back
+            </button>
+        </NuxtLink>
+    </main>
+</template>
+
+<script> 
+export default {
+
+}
+</script>
