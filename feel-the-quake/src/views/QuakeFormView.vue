052e40f3fb1191552acdab23572c6f00365aeb1c
Kardam Simeonov <masutayodagm@gmail.com>
2023-06-19 14:03:14 +0300

    discarded changes

--- feel-the-quake/src/views/QuakeFormView.vue
+++ feel-the-quake/src/views/QuakeFormView.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="flex justify-center items-center h-full min-h-screen bg-cyan-100">
+  <main class="flex justify-center items-center h-full min-h-screen bg-blue-300">
     <div
-      class="relative flex flex-col h-fit w-full pb-12 pt-16 px-6 mx-4 bg-white border-[1.5px] border-b-[6px] border-cyan-500 shadow-lg rounded-xl">
+      class="relative flex flex-col h-fit w-full pb-12 pt-16 px-6 mx-4 bg-white border-[1.5px] border-b-[6px] border-sky-200 shadow-lg rounded-xl">
       <button type="button" v-if="formIndex > 0 && formIndex < 4" @click="formIndex--"
-        class="absolute top-2 left-3 flex items-center justify-center aspect-square rounded-full w-10 text-xl text-center text-cyan-500 border-[1px] border-neutral-200 shadow">
+        class="absolute top-2 left-3 flex items-center justify-center aspect-square rounded-full w-10 text-xl text-center text-cyan-600 border-[1px] border-neutral-200 shadow">
         <Icon icon="fa6-solid:chevron-left" />
       </button>
       <RouterLink v-else-if="formIndex < 4" to="/hub">
@@ -21,7 +21,7 @@
             Tiles &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors
           </div>
           <button type="button" @click="formIndex++"
-            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-cyan-200 bg-cyan-500 font-semibold text-white text-xl w-fit shadow-md"
+            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-sky-200 bg-sky-600 font-semibold text-white text-xl w-fit shadow-md"
             :class="{ 'invisible': markerLocation == null }">
             Напред
             <Icon class="my-auto" icon="fa6-solid:chevron-right" />
@@ -43,7 +43,7 @@
           </div>
           <div class="h-[0.10rem] w-1/2 bg-sky-100 my-8"></div>
           <button type="button" @click="formIndex++"
-            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-cyan-200 bg-cyan-500 font-semibold text-white text-xl w-fit shadow-md">
+            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-sky-200 bg-sky-600 font-semibold text-white text-xl w-fit shadow-md">
             Напред
             <Icon class="my-auto" icon="fa6-solid:chevron-right" />
           </button>
@@ -56,16 +56,16 @@
           <ul class="flex gap-3 w-fit mb-6">
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[0] = 1" :class="{ 'bg-teal-100': selectionIndecies[0] >= 1 }">1</li>
+              @click="selectionIndecies[0] = 1" :class="{ 'bg-emerald-300': selectionIndecies[0] >= 1 }">1</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[0] = 2" :class="{ 'bg-teal-200': selectionIndecies[0] >= 2 }">2</li>
+              @click="selectionIndecies[0] = 2" :class="{ 'bg-teal-300': selectionIndecies[0] >= 2 }">2</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[0] = 3" :class="{ 'bg-teal-300': selectionIndecies[0] >= 3 }">3</li>
+              @click="selectionIndecies[0] = 3" :class="{ 'bg-sky-300': selectionIndecies[0] >= 3 }">3</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[0] = 4" :class="{ 'bg-teal-400': selectionIndecies[0] >= 4 }">4</li>
+              @click="selectionIndecies[0] = 4" :class="{ 'bg-blue-300': selectionIndecies[0] >= 4 }">4</li>
           </ul>
           <div class="text-center bg-cyan-100 p-4 rounded-xl min-h-[8rem]">
             <h2 class="text-xl font-semibold text-cyan-600 mb-2">{{ intensity[selectionIndecies[0]].title }}</h2>
@@ -73,7 +73,7 @@
           </div>
           <div class="h-[0.10rem] w-1/2 bg-sky-100 my-8"></div>
           <button type="button" @click="formIndex++"
-            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-cyan-200 bg-cyan-500 font-semibold text-white text-xl w-fit shadow-md">
+            class="relative flex gap-2 rounded-xl py-2 px-6 border-2 border-sky-200 bg-sky-600 font-semibold text-white text-xl w-fit shadow-md">
             Напред
             <Icon class="my-auto" icon="fa6-solid:chevron-right" />
           </button>
@@ -86,16 +86,16 @@
           <ul class="flex gap-3 w-fit mb-6">
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[3] = 1" :class="{ 'bg-teal-100': selectionIndecies[3] >= 1 }">1</li>
+              @click="selectionIndecies[3] = 1" :class="{ 'bg-emerald-300': selectionIndecies[3] >= 1 }">1</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[3] = 2" :class="{ 'bg-teal-200': selectionIndecies[3] >= 2 }">2</li>
+              @click="selectionIndecies[3] = 2" :class="{ 'bg-teal-300': selectionIndecies[3] >= 2 }">2</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[3] = 3" :class="{ 'bg-teal-300': selectionIndecies[3] >= 3 }">3</li>
+              @click="selectionIndecies[3] = 3" :class="{ 'bg-sky-300': selectionIndecies[3] >= 3 }">3</li>
             <li
               class="flex items-center justify-center aspect-square rounded-full w-10 text-2xl text-center font-bold text-cyan-700 border-[1px] border-neutral-200 shadow"
-              @click="selectionIndecies[3] = 4" :class="{ 'bg-teal-400': selectionIndecies[3] >= 4 }">4</li>
+              @click="selectionIndecies[3] = 4" :class="{ 'bg-blue-300': selectionIndecies[3] >= 4 }">4</li>
           </ul>
           <div class="text-center bg-cyan-100 p-4 rounded-xl min-h-[8rem]">
             <h2 class="text-xl font-semibold text-cyan-600 mb-2">{{ damage[selectionIndecies[3]].title }}</h2>
@@ -103,7 +103,7 @@
           </div>
           <div class="h-[0.10rem] w-1/2 bg-sky-100 my-8"></div>
           <button type="button" @click="formIndex++"
-            class="relative rounded-xl py-2 px-6 border-2 border-cyan-200 bg-cyan-500 font-semibold text-white text-xl w-fit shadow-md">
+            class="relative rounded-xl py-2 px-6 border-2 border-sky-200 bg-sky-600 font-semibold text-white text-xl w-fit shadow-md">
             Предаване
           </button>
         </form>
@@ -119,21 +119,21 @@
           <div class="h-[0.10rem] w-1/2 bg-sky-100 mb-8 mt-16"></div>
           <RouterLink to="/hub">
             <button type="button" @click="turnInForm()"
-              class="relative rounded-xl py-2 px-6 border-2 border-cyan-200 bg-teal-500 font-semibold text-white text-xl w-fit shadow-md">
+              class="relative rounded-xl py-2 px-6 border-2 border-sky-200 bg-sky-600 font-semibold text-white text-xl w-fit shadow-md">
               Връщане обратно
             </button>
           </Routerlink>
         </form>
       </section>
       <div v-show="formIndex < 4" class="flex gap-2 absolute w-fit left-0 right-0 mx-auto top-[95%]">
-        <div class="aspect-square w-[0.60rem] border-[1px] border-cyan-500 rounded-full"
-          :class="{ 'bg-cyan-500': formIndex >= 0 }"></div>
-        <div class="aspect-square w-[0.60rem] border-[1px] border-cyan-500 rounded-full"
-          :class="{ 'bg-cyan-500': formIndex >= 1 }"></div>
-        <div class="aspect-square w-[0.60rem] border-[1px] border-cyan-500 rounded-full"
-          :class="{ 'bg-cyan-500': formIndex >= 2 }"></div>
-        <div class="aspect-square w-[0.60rem] border-[1px] border-cyan-500 rounded-full"
-          :class="{ 'bg-cyan-500': formIndex >= 3 }"></div>
+        <div class="aspect-square w-[0.60rem] border-[1px] border-neutral-300 rounded-full"
+          :class="{ 'bg-sky-300': formIndex >= 0 }"></div>
+        <div class="aspect-square w-[0.60rem] border-[1px] border-neutral-300 rounded-full"
+          :class="{ 'bg-sky-300': formIndex >= 1 }"></div>
+        <div class="aspect-square w-[0.60rem] border-[1px] border-neutral-300 rounded-full"
+          :class="{ 'bg-sky-300': formIndex >= 2 }"></div>
+        <div class="aspect-square w-[0.60rem] border-[1px] border-neutral-300 rounded-full"
+          :class="{ 'bg-sky-300': formIndex >= 3 }"></div>
       </div>
     </div>
   </main>
