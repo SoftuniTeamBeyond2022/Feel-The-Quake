73954b8c43a7db54fa496a298574396909ab62b5
KardamSimeonov <masutayodagm@gmail.com>
2023-06-25 20:21:36 +0300

    tailwind colors

--- feel-the-quake/tailwind.config.js
+++ feel-the-quake/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        quakeGreen: {
+          background: '#b3ded8',
+          dark: '#164e48',
+          light: '#81aba5',
+          DEFAULT: '#00917d',
+          '100': '#a1b7b5',
+          '200': '#71938f',
+          '300': '#43706b',
+          '400': '#154d47',
+        },
+        quakeRed: {
+          light: '#c17777',
+          dark: '#a73b3b',
+        },
+      },
+    },
   },
   plugins: [],
 }
